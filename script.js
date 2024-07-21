dce07b50449a720788e57e87abec99456382982e
youssefazer97 <yoosha1997@gmail.com>
2024-07-22 00:40:49 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   const addButton = document.getElementById("add-task-btn");
   const taskInput = document.getElementById("task-input");
   const taskList = document.getElementById("task-list");
